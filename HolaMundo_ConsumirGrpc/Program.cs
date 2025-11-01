01936c0df1a65b7b33a21808122176ecc8f49f4e
Víctor Martínez <ahal_tocob@hotmail.com>
2025-10-31 20:36:11 -0600

    grcp

---
+++ HolaMundo_ConsumirGrpc/Program.cs
@@ -0,0 +1,15 @@
+﻿using Grpc.Net.Client;
+using HolaMundo_Grpc;
+
+Console.WriteLine("Consumir el gPRC");
+
+using var channel = GrpcChannel.ForAddress("http://localhost:5000");
+
+var client = new Greeter.GreeterClient(channel);
+
+Console.WriteLine("Ingrese su nombre:");
+var nombre = Console.ReadLine();
+
+var reply = await client.SayHelloAsync(new HelloRequest { Name = nombre });
+
+Console.WriteLine("Saludo recibido: " + reply.Message);
